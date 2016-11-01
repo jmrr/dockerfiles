8feaab8b2f0f36ef1209276d53f389ee1468a68f
Jose Rivera <josemrivera@gmail.com>
2016-11-01 10:17:27 +0000

    Not fully functional image + startup script

--- postgres-node/startup.sh
+++ postgres-node/startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-npm install
-
 su postgres -c "/usr/lib/postgresql/9.5/bin/postgres -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf"
 
+su postgres -c 'psql --command "ALTER USER $PGUSER WITH PASSWORD '$PGPASSWORD';"'
+
 su postgres -c "/blackbox/ngrok http 3000 --config=ngrok.yml > /dev/null &"
+
+export SERVER_HOST=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')  
+
+npm install
