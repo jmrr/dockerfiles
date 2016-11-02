593c784b754c8b9910d8f58b14670f18d5d253e6
Jose Rivera <josemrivera@gmail.com>
2016-11-02 10:30:08 +0000

    Adding SERVER_HOST env var retrieval and db initialisation scripts

--- node-ngrok/startup.sh
+++ node-ngrok/startup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 # Run ngrok to expose Node.js web server port
-ngrok http 3000 --config=/ngrok.yml > /dev/null 
+ngrok http 3000 --config=/ngrok.yml > /dev/null & 
+sleep 2
+
+# Set the SERVER_HOST env variable
+export SERVER_HOST=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')  
+
+# Create tables
+node create-tables.js
+# Create API key
+node scripts/createApiKey.js --role rw --key testkey
+
+# Run app
+
+node app.js
