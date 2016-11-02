6d628729bfbebb5fe3c979506b2d226b0b780c3b
Jose Rivera <josemrivera@gmail.com>
2016-11-02 12:10:30 +0000

    Simplified startup script

--- node-ngrok/startup.sh
+++ node-ngrok/startup.sh
@@ -7,11 +7,5 @@ sleep 2
 # Set the SERVER_HOST env variable
 export SERVER_HOST=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')  
 
-# Create tables
-node create-tables.js
-# Create API key
-node scripts/createApiKey.js --role rw --key testkey
-
 # Run app
-
 node app.js
