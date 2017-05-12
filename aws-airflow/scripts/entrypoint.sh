1389367e73f25762861e1e9bc5157bb935ca413f
Jose Rivera <josemrivera@gmail.com>
2017-05-12 09:19:31 +0100

    Add airflow based on AWS current AMI

---
+++ aws-airflow/scripts/entrypoint.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+AIRFLOW_HOME="/usr/local/airflow"
+CMD="airflow"
+TRY_LOOP="20"
+
+
+: ${POSTGRES_HOST:="postgres"}
+: ${POSTGRES_PORT:="5432"}
+: ${POSTGRES_USER:="airflow"}
+: ${POSTGRES_PASSWORD:="airflow"}
+: ${POSTGRES_DB:="airflow"}
+
+# Fernet secret key for encryption when saving connection passwords in the db
+: ${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
+
+# Load DAGs exemples (default: Yes)
+if [ "$LOAD_EX" = "n" ]; then
+    sed -i "s/load_examples = True/load_examples = False/" "$AIRFLOW_HOME"/airflow.cfg
+fi
+
+# Install custom python package if requirements.txt is present
+if [ -e "/usr/local/predict/requirements.txt" ]; then
+    echo "Installing Python dependencies"
+    $(which pip) install --user -r /usr/local/predict/requirements.txt
+fi
+
+# Install Predict as an editable package
+if [ -e "/usr/local/predict/setup.py" ]; then
+    echo "Installing Predict"
+    $(which pip) install --user -e /usr/local/predict/
+fi
+
+
+
+# Update airflow config - Fernet key
+sed -i "s|\$FERNET_KEY|$FERNET_KEY|" "$AIRFLOW_HOME"/airflow.cfg
+
+# Wait for Postresql
+if [ "$1" = "webserver" ] ; then
+  i=0
+  while ! ncat $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
+    i=$((i+1))
+    if [ "$1" = "webserver" ]; then
+      echo "$(date) - waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
+      if [ $i -ge $TRY_LOOP ]; then
+        echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} still not reachable, giving up"
+        exit 1
+      fi
+    fi
+    sleep 10
+  done
+fi
+
+echo "$(date) - using LocalExecutor as default"
+
+sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" "$AIRFLOW_HOME"/airflow.cfg
+
+echo "Initialize database..."
+$CMD initdb
+exec $CMD webserver &
+exec $CMD scheduler
